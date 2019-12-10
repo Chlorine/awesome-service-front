68d143e050bd25297bf5707fa34af89b258fdf55
ts.s.khlobystov@gmail.com <ts.s.khlobystov@gmail.com>
2019-12-10 12:35:34 +0300

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,8 @@ const App: React.FC = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Крутись против часовой!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
